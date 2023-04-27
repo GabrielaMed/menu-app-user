d525d2fd85ef4f35fd87305141ba587823447959
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-04-27 17:27:48 -0400

    finish product page and start cart

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,45 @@
+import { MdArrowBack } from 'react-icons/md';
+import { Container, Navbar } from './style';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ToastMessage } from '../../components/Toast';
+import { IToastType } from '../../utils/Interface/Toast';
+import { api } from '../../services/api';
+import { AxiosError } from 'axios';
+
+interface Props {
+  orderId?: string;
+}
+
+export const Cart = ({ orderId }: Props) => {
+  const navigate = useNavigate();
+  const { companyId } = useParams();
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessageType, setToastMessageType] = useState<IToastType>(
+    IToastType.unknow
+  );
+  const [toastMessage, setToastMessage] = useState('');
+
+  return (
+    <>
+      <ToastMessage
+        setShowToast={setShowToast}
+        showToast={showToast}
+        toastMessage={toastMessage}
+        toastMessageType={toastMessageType}
+      />
+      <Container>
+        <Navbar>
+          <span>
+            <MdArrowBack
+              size={24}
+              style={{ cursor: 'pointer' }}
+              onClick={() => navigate(`/${companyId}}`)}
+            />
+          </span>
+          <span>Carrinho</span>
+        </Navbar>
+      </Container>
+    </>
+  );
+};
