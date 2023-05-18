680094e48d16273b49d0266eb7e2f8c67de73952
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-05-18 14:32:55 -0400

    add pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(<App />);
+
+serviceWorkerRegistration.unregister();
