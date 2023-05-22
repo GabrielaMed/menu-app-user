6297f6b6523e0787597e4bd19b3f2444be5f834d
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-05-22 16:12:02 -0400

    undo network first

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -16,48 +16,27 @@ self.addEventListener('install', (e) => {
 });
 
 // listen for request
-// self.addEventListener('fetch', (evt) => {
-//   // check if request is made by chrome extensions or web page
-//   // if request is made for web page url must contains http.
-//   if (!(evt.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
+self.addEventListener('fetch', (evt) => {
+  // check if request is made by chrome extensions or web page
+  // if request is made for web page url must contains http.
+  if (!(evt.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
 
-//   evt.respondWith(
-//     caches
-//       .match(evt.request)
-//       .then(
-//         (cacheRes) =>
-//           cacheRes ||
-//           fetch(evt.request).then((fetchRes) =>
-//             caches.open(CACHE_NAME).then((cache) => {
-//               cache.put(evt.request.url, fetchRes.clone());
-//               // check cached items size
-//               limitCacheSize(CACHE_NAME, 75);
-//               return fetchRes;
-//             })
-//           )
-//       )
-//       .catch(() => caches.match('/fallback'))
-//   );
-// });
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Check if the requested resource is available in the cache
-      if (response) {
-        return response; // Return the cached response
-      }
-
-      // If the resource is not available in the cache, fetch it from the network
-      return fetch(event.request).then(function (networkResponse) {
-        // Cache the fetched response for future use
-        caches.open('my-cache').then(function (cache) {
-          cache.put(event.request, networkResponse.clone());
-        });
-
-        return networkResponse; // Return the fetched response
-      });
-    })
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then(
+        (cacheRes) =>
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) =>
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              limitCacheSize(CACHE_NAME, 75);
+              return fetchRes;
+            })
+          )
+      )
+      .catch(() => caches.match('/fallback'))
   );
 });
 
