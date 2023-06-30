17dcc8d081053481bd97c63253eb4cff70f10897
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-06-30 14:37:56 -0400

    remove unused imports

--- src/utils/Interface/Order.ts
+++ src/utils/Interface/Order.ts
@@ -1,7 +1,5 @@
 import { OrderStatus } from '../Enum/OrderStatus';
-import { IAdditional } from './Additional';
 import { IOrderProduct } from './OrderProduct';
-import { IProduct } from './Product';
 
 export interface IOrder {
   id: string;
